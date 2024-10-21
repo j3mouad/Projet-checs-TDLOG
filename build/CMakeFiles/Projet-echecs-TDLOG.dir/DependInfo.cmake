c7022ccabb4eb7e849a5de8b88933dd37b4b16d3
Mouad JBILI <151060921+j3mouad@users.noreply.github.com>
2024-10-21 15:26:18 +0200

    first commit

---
+++ build/CMakeFiles/Projet-echecs-TDLOG.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/jmoua/OneDrive/Documents/GitHub/Projet-checs-TDLOG/main.cpp" "CMakeFiles/Projet-echecs-TDLOG.dir/main.cpp.obj" "gcc" "CMakeFiles/Projet-echecs-TDLOG.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
